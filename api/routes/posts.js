8e7f5b07fcc1853a6c3e68c12616eae370a204d9
Shamima14 <sbegum91@gmail.com>
2023-03-09 16:28:01 +0000

    api findById created and tested-passed

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,5 +5,6 @@ const PostsController = require("../controllers/posts");
 
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
+router.get("/:postID", PostsController.FindById);
 
 module.exports = router;
