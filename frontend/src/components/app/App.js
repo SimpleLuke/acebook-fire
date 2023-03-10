a5fc31c75f6536e5f14e53844aef8da4413c6865
Shamima14 <sbegum91@gmail.com>
2023-03-10 23:10:59 +0000

    Resolved all missing code, bugs in feed test

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
-import PostById from '../PostById/postById';
+import PostById from '../postById/PostById';
 import React, { useState } from 'react';
 import Feed from '../feed/Feed'
 import {
