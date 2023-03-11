af38ba251e3d5d62c56d058389c8153e91eba122
Luke Lai <lukelai.dev@gmail.com>
2023-03-11 23:47:43 +0000

    Fix fetching post. Instead of refresh the page after submit new post, now it fetches posts again from the server.

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,26 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import Post from '../post/Post'
-import Navbar from '../navbar/Navbar';
-import './Feed.css'
+import React, { useEffect, useState } from "react";
+import Post from "../post/Post";
+import Navbar from "../navbar/Navbar";
+import "./Feed.css";
 
-const Feed = ({ navigate,userData,storeUserData }) => {
+const Feed = ({ navigate, userData, storeUserData }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
   const [newPost, setNewPost] = useState("");
 
+  const fetchPosts = () => {
+    fetch("/posts", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(async (data) => {
+        window.localStorage.setItem("token", data.token);
+        setToken(window.localStorage.getItem("token"));
+        setPosts(data.posts);
+      });
+  };
+
   useEffect(() => {
     if (token) {
-      fetch("/posts", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then(async (data) => {
-          window.localStorage.setItem("token", data.token);
-          setToken(window.localStorage.getItem("token"));
-          setPosts(data.posts);
-        });
+      fetchPosts();
     }
   }, []);
 
@@ -34,16 +38,18 @@ const Feed = ({ navigate,userData,storeUserData }) => {
       },
       body: JSON.stringify({ message: newPost }),
     });
-    setPosts([...posts, newPost]);
     setNewPost("");
-    navigate(0);
+    fetchPosts();
   };
-
 
   if (token) {
     return (
       <>
-        <Navbar navigate={navigate} userData={userData} storeUserData={storeUserData}/>
+        <Navbar
+          navigate={navigate}
+          userData={userData}
+          storeUserData={storeUserData}
+        />
         <h2>Posts</h2>
         <form onSubmit={handleSubmit}>
           <label>
@@ -69,7 +75,6 @@ const Feed = ({ navigate,userData,storeUserData }) => {
   } else {
     navigate("/signin");
   }
-  
-}
+};
 
 export default Feed;
