5355396c41254d9f7cb1126ad610205c01f5c76d
James Mcleish <119613083+jamesmcleish@users.noreply.github.com>
2023-03-14 12:02:02 +0000

    Update users.js

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -11,18 +11,6 @@ const UsersController = {
       }
     });
   },
-  // GetUser: async (req, res) => {
-  //   const { email } = req.params;
-  //   try {
-  //     const user = await User.findOne({ email });
-  //     if (!user) {
-  //       return res.status(404).json({ message: "User not found" });
-  //     }
-  //     res.status(200).json(user);
-  //   } catch (err) {
-  //     res.status(500).json({ error: err.message });
-  //   }
-  // }
   GetUser: async (req, res) => {
     try {
       const email = req.query.email;
