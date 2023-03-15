f8386e5ec97308b88df7f1cdfa914fa455c5a0e1
Luke Lai <lukelai.dev@gmail.com>
2023-03-15 22:26:57 +0000

    Add cypress-mongodb package to drop collection during cypress e2e test. Fix some tests error.

--- frontend/cypress/e2e/like_button.cy.js
+++ frontend/cypress/e2e/like_button.cy.js
@@ -1,4 +1,4 @@
-beforeEach( () => {
+beforeEach(() => {
   cy.visit("/signup");
   cy.signup("name", "surname", "someone@example.com", "password");
   cy.visit("/login");
@@ -7,28 +7,35 @@ beforeEach( () => {
   cy.get("#submit").click();
   cy.wait(1000);
   cy.visit("/posts");
- })
+});
 
- describe('Post', () => {
-  it('toggles like button', () => {
+afterEach(() => {
+  cy.dropCollection("users", { failSilently: true }).then((res) => {
+    cy.log(res); // prints 'Collection dropped'
+  });
+});
+
+describe("Post", () => {
+  it("toggles like button", () => {
     cy.get('[data-cy="likeButton"]').first().click();
-    cy.get('[data-cy="likeButton"]').first().should('contain.text', 'Unlike');
+    cy.get('[data-cy="likeButton"]').first().should("contain.text", "Unlike");
     cy.get('[data-cy="likeButton"]').first().click();
-    cy.get('[data-cy="likeButton"]').first().should('contain.text', 'Like');
+    cy.get('[data-cy="likeButton"]').first().should("contain.text", "Like");
     cy.logout();
-  })
+  });
 
   it("sign up, login and make a new post", () => {
     cy.get('input[data-cy="post-input"]').first().type("This is a new post");
     cy.get('button[data-cy="form-submit"]').first().click();
     cy.wait(1000);
-    cy.get('[data-cy="post"]').first().should("contain.text", "This is a new post");
+    cy.get('[data-cy="post"]')
+      .first()
+      .should("contain.text", "This is a new post");
     cy.get('[data-cy="post"]').first().should("contain.text", "name surname");
-    cy.get('[data-cy="post"]').first().should("contain.text", '2023');
+    cy.get('[data-cy="post"]').first().should("contain.text", "2023");
 
-    cy.get('[data-cy="like-element"]').first().should("contain.text", '0 likes');
+    cy.get('[data-cy="like-element"]')
+      .first()
+      .should("contain.text", "0 likes");
   });
-
-  
-  
- })
+});
