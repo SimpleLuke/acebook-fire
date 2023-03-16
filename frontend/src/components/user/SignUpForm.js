5a66a1917cab82f7b534a57c917a349408fdb335
Shamima14 <sbegum91@gmail.com>
2023-03-16 22:12:53 +0000

    first styling of signup page

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+
 const SignUpForm = ({ navigate }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -55,37 +56,106 @@ const SignUpForm = ({ navigate }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="First Name"
-        id="firstName"
-        type="text"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last Name"
-        id="lastName"
-        type="text"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <input
-        placeholder="Email"
-        id="email"
-        type="text"
-        value={email}
-        onChange={handleEmailChange}
-      />
-      <input
-        placeholder="Password"
-        id="password"
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-      />
-      <input id="submit" type="submit" value="Submit" />
-    </form>
+    <div class="bg-yellow-50">
+    <div className="p-28 rounded-lg">
+      <div className="flex min-h-full">
+            <div className="flex flex-1 flex-col justify-center py-12 px-4 lg:px-6 lg:flex-none lg:px-20 xl:px-24">
+              <div className="mx-auto w-full max-w-lg lg:w-96">
+                <div>
+                  <img
+                    className="h-12 w-auto"
+                    src="./burnbook.png"
+                    alt="burn-book"
+                  />
+                  <h2 className="mt-6 text-3xl font-bold tracking-tight text-red-600">Sign Up</h2>
+                  
+                </div >
+                <div className="flex p-4 mt-6">
+                  <form onSubmit={handleSubmit}>
+                  <div>
+                    <label className="block text-lg font-medium leading-6 text-gray-900">
+                      First Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        id="firstName"
+                        type="text"
+                        value={firstName}
+                        onChange={handleFirstNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className="block text-lg font-medium leading-6 text-gray-900">
+                      Last Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        id="lastName"
+                        type="text"
+                        value={lastName}
+                        onChange={handleLastNameChange}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className="block text-lg font-medium leading-6 text-gray-900">
+                      Email
+                    </label>
+                    <div className="mb-2">
+                      <input
+                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        id="email"
+                        type="text"
+                        value={email}
+                        onChange={handleEmailChange}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className="block text-lg font-medium leading-6 text-gray-900">
+                      Password
+                    </label>
+                    <input
+                      className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                      id="password"
+                      type="password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </div>
+
+                  
+                  <div class="mt-12" >
+                    <input 
+                      className="flex w-full justify-center rounded-lg bg-red-400 py-2 px-3 text-lg font-semibold text-white shadow-lg hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      id="submit" 
+                      type="submit" 
+                      value="Submit"
+                    />
+                    
+                  </div>
+                </form>
+
+
+                </div>
+              </div>
+            </div>
+            <div className="relative hidden w-0 flex-1 lg:block flex min-h-full">
+                  <img
+                    className="absolute inset-100 h-full w-full object-cover"
+                    src="./bookflame.png"//heerreee
+                    alt=""
+                  />
+            </div>
+          </div>
+        </div>  
+    </div>
+    
+
+    
   );
 };
 
