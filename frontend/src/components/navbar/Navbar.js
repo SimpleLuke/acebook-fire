768f2e8473d3c70cbabd1bc13b250428bd5410ea
annamaggg <annamagnusson99@gmail.com>
2023-03-16 19:10:52 +0000

    colour scheme change

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = ({ navigate, userData, storeUserData }) => {
   };
 
   return (
-    <nav className="h-16 p2 mx-auto place-content-between relative flex px-5 flex bg-gray-800 flex items-center">
+    <nav className="h-16 p2 pt-9 pb-9 mx-auto place-content-between relative flex px-5 flex bg-red-200 flex items-center">
       <div
         className="w-48 text-lg text-white cursor-pointer"
         data-cy="homeButton"
@@ -18,11 +18,11 @@ const Navbar = ({ navigate, userData, storeUserData }) => {
         <img src={logo} alt="LogoIcon" />
       </div>
       <div className="flex justify-between">
-      <div className="p-2 right-0 text-lg text-white cursor-default " data-cy="user-first-name">
+      <div className="p-2 right-0 text-lg text-red-500 cursor-default " data-cy="user-first-name">
         {userData ? userData.firstName : ""}
       </div>
       <div
-        className="p-2 px-4 left-0 text-white cursor-pointer hover:bg-gray-500 relative ml-3 flex rounded-3xl bg-gray-600  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+        className="p-2 px-4 left-0 text-white cursor-pointer hover:bg-red-500 relative ml-3 flex rounded-3xl bg-red-400  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
         // text-sm
         //className="p-3 text-lg cursor-pointer border rounded-t bg-yellow-300 hover:bg-yellow-600"
         data-cy="logoutButton"
