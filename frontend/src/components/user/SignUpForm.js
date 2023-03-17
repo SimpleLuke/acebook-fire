e092a7dee6a6bccecc2497a5024707294dbb2dcf
Shamima14 <sbegum91@gmail.com>
2023-03-17 11:56:54 +0000

    current styling of signup page

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -56,29 +56,29 @@ const SignUpForm = ({ navigate }) => {
   };
 
   return (
-    <div class="bg-yellow-50">
-    <div className="p-28 rounded-lg">
+    <div>
+    <div class="bg-body-background">
+    <div className="p-20 mx-w-md rounded-lg ">
       <div className="flex min-h-full">
-            <div className="flex flex-1 flex-col justify-center py-12 px-4 lg:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div className="flex flex-1 flex-col justify-center py-12 px-4 lg:px-6 lg:flex-none lg:px-20 xl:px-24 bg-white rounded-2xl">
               <div className="mx-auto w-full max-w-lg lg:w-96">
                 <div>
+
                   <img
-                    className="h-12 w-auto"
+                    className="h-auto w-auto"
                     src="./burnbook.png"
                     alt="burn-book"
                   />
-                  <h2 className="mt-6 text-3xl font-bold tracking-tight text-red-600">Sign Up</h2>
+                  <h2 className="mt-6 translate-x-32 text-3xl font-bold tracking-tight text-red-600">Sign Up</h2>
                   
                 </div >
                 <div className="flex p-4 mt-6">
                   <form onSubmit={handleSubmit}>
                   <div>
-                    <label className="block text-lg font-medium leading-6 text-gray-900">
-                      First Name
-                    </label>
                     <div className="mt-2">
                       <input
-                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        className="translate-x-14 block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        placeholder="First Name"
                         id="firstName"
                         type="text"
                         value={firstName}
@@ -87,12 +87,10 @@ const SignUpForm = ({ navigate }) => {
                     </div>
                   </div>
                   <div>
-                    <label className="block text-lg font-medium leading-6 text-gray-900">
-                      Last Name
-                    </label>
                     <div className="mt-2">
                       <input
-                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        className="translate-x-14 block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        placeholder="Last Name"
                         id="lastName"
                         type="text"
                         value={lastName}
@@ -101,12 +99,10 @@ const SignUpForm = ({ navigate }) => {
                     </div>
                   </div>
                   <div>
-                    <label className="block text-lg font-medium leading-6 text-gray-900">
-                      Email
-                    </label>
-                    <div className="mb-2">
+                    <div className="mt-2">
                       <input
-                        className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        className="translate-x-14 block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                        placeholder="Email"
                         id="email"
                         type="text"
                         value={email}
@@ -114,12 +110,10 @@ const SignUpForm = ({ navigate }) => {
                       />
                     </div>
                   </div>
-                  <div>
-                    <label className="block text-lg font-medium leading-6 text-gray-900">
-                      Password
-                    </label>
+                  <div className="mt-2">
                     <input
-                      className="block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                      className="translate-x-14 block w-full rounded-lg border-0 py-1.5 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 lg:text-lg lg:leading-6"
+                      placeholder="Password"
                       id="password"
                       type="password"
                       value={password}
@@ -128,9 +122,9 @@ const SignUpForm = ({ navigate }) => {
                   </div>
 
                   
-                  <div class="mt-12" >
+                  <div class="mt-2" >
                     <input 
-                      className="flex w-full justify-center rounded-lg bg-red-400 py-2 px-3 text-lg font-semibold text-white shadow-lg hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="translate-x-14 flex w-full justify-center rounded-lg bg-red-400 py-2 px-3 text-lg font-semibold text-white shadow-lg hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       id="submit" 
                       type="submit" 
                       value="Submit"
@@ -144,14 +138,16 @@ const SignUpForm = ({ navigate }) => {
               </div>
             </div>
             <div className="relative hidden w-0 flex-1 lg:block flex min-h-full">
-                  <img
+               <img
                     className="absolute inset-100 h-full w-full object-cover"
                     src="./bookflame.png"//heerreee
                     alt=""
                   />
+                 
             </div>
           </div>
         </div>  
+    </div>
     </div>
     
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var'],
+      },
+      backgroundImage: {
+        'body-background': "url(‘../public/img/burnbookbg.jpg')",
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
   ],
-}
+};
